cdb62c80f36586e8176c5c2c2faaf80b925bf699
vudiep411 <vudiep411@gmail.com>
2022-09-21 00:35:16 -0700

    json

--- main.cpp
+++ main.cpp
@@ -2,9 +2,32 @@
 
 using namespace std;
 
+struct json
+{
+    std::string key;
+    std::string value;
+    json(){}
+    json(const string& k, const string& val) : key(k), value(val){}
+    json& operator=(const json& rhs)
+    {
+        key = rhs.key;
+        value = rhs.value;
+        return *this;
+    }
+};
+
+std::ostream& operator<<(std::ostream& os, const json& j)
+{
+    std::cout << "{ " << j.key << ": " << j.value << " }" << std::endl;
+    return os;
+}
+
+
 extern void print(const string&);
 int main()
 {
-    print("file.txt");
+    //print("file.txt");
+    json j("key", "value");
+    cout << j.value << endl;
     return 0;
 }
